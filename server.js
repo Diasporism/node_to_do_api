544bc5f102f208b85fab820bacd9daf658fb5130
Logan Sears <lsears322@gmail.com>
2014-08-19 20:27:37 -0600

    Initial commit for Node.js to do app.

---
+++ server.js
@@ -0,0 +1,18 @@
+var express        = require('express');
+var app            = express();
+var mongoose       = require('mongoose');
+var morgan         = require('morgan');
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
+
+mongoose.connect('mongodb://lsears:password@proximus.modulusmongo.net:27017/o2jYmyhi')
+
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(methodOverride());
+
+app.listen(3000);
+console.log('App listening on port 3000.');
