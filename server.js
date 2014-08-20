3e2705b7da1d7c1dd3aa065b5021f116876e9278
Logan Sears <lsears322@gmail.com>
2014-08-19 20:42:56 -0600

    Add get, create, and delete api endpoints for todo objects.

--- server.js
+++ server.js
@@ -14,5 +14,42 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(methodOverride());
 
+var Todo = mongoose.model('Todo', {
+  text : String
+});
+
+app.get('/api/todos', function(req, res) {
+  Todo.find(function(err, todos) {
+    if (err)
+      res.send(err)
+    res.json(todos);
+  });
+});
+
+app.post('/api/todos', function(req, res) {
+  Todo.create({
+    text : req.body.text,
+    done : false
+  }, function(err, todo){
+    if (err)
+      res.send(err)
+    res.json(todo);
+  });
+});
+
+app.delete('/api/todos/:todo_id', function(req, res) {
+  Todo.remove({
+    _id : req.params.todo_id
+  }, function(err, todo){
+    if (err)
+      res.send(err)
+    Todo.find(function(err, todos) {
+      if(err)
+        res.send(err)
+      res.json(todos)
+    });
+  });
+});
+
 app.listen(3000);
 console.log('App listening on port 3000.');
